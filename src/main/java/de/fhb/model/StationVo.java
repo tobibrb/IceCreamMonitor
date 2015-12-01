6a9bacb2181a0c23d4e9ed8d0bf4d479aba3c787
Toni <Toni@172.16.25.207>
2015-12-01 13:32:59 +0100

    MonitorShowtDataView

--- src/main/java/de/fhb/model/StationVo.java
+++ src/main/java/de/fhb/model/StationVo.java
@@ -1,5 +1,7 @@
 package de.fhb.model;
 
+import javafx.beans.property.SimpleStringProperty;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -16,11 +18,22 @@ public class StationVo implements Serializable {
     private Integer variance;
     private Date date;
 
+    private SimpleStringProperty nameSSP, dateSSP, targetSSP, actualSSP, varianceSSP;
+
+
+
     public StationVo(String name, Integer targetValue, Integer actualValue, Integer variance, Date date) {
         this(name, targetValue);
         this.actualValue = actualValue;
         this.variance = variance;
         this.date = date;
+
+        this.nameSSP = new SimpleStringProperty(name);
+        this.targetSSP = new SimpleStringProperty(targetValue.toString());
+        this.actualSSP = new SimpleStringProperty(actualValue.toString());
+        this.varianceSSP = new SimpleStringProperty(variance.toString());
+        this.dateSSP = new SimpleStringProperty(date.toString());
+
     }
 
     public StationVo(String name, Integer targetValue) {
@@ -29,6 +42,13 @@ public class StationVo implements Serializable {
         this.targetValue = targetValue;
         this.date = new Date();
         nextId++;
+
+        this.nameSSP = new SimpleStringProperty(name);
+        this.targetSSP = new SimpleStringProperty(this.targetValue.toString());
+        this.dateSSP = new SimpleStringProperty(this.date.toString());
+        this.actualSSP = new SimpleStringProperty(null);
+        this.varianceSSP = new SimpleStringProperty(null);
+
     }
 
     public Long getId() {
@@ -57,18 +77,42 @@ public class StationVo implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+        this.nameSSP.set(name);
     }
 
     public void setActualValue(Integer actualValue) {
         this.actualValue = actualValue;
+        this.actualSSP.set(actualValue.toString());
     }
 
     public void setDate(Date date) {
         this.date = date;
+        this.dateSSP.set(date.toString());
     }
 
     protected void setVariance(Integer variance) {
         this.variance = variance;
+        this.varianceSSP.set(variance.toString());
+    }
+
+    public SimpleStringProperty nameSSPProperty() {
+        return nameSSP;
+    }
+
+    public SimpleStringProperty dateSSPProperty() {
+        return dateSSP;
+    }
+
+    public SimpleStringProperty targetSSPProperty() {
+        return targetSSP;
+    }
+
+    public SimpleStringProperty actualSSPProperty() {
+        return actualSSP;
+    }
+
+    public SimpleStringProperty varianceSSPProperty() {
+        return varianceSSP;
     }
 
     @Override
--- src/main/java/de/fhb/view/MonitorShowDataView.java
+++ src/main/java/de/fhb/view/MonitorShowDataView.java
@@ -1,16 +1,23 @@
 package de.fhb.view;
 
 import de.fhb.model.StationVo;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Callback;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -22,17 +29,17 @@ public class MonitorShowDataView extends AMonitorView implements Initializable {
     @FXML
     private Button changeViewBtn;
     @FXML
-    private TableView tabelView;
+    private TableView<StationVo> tableView;
     @FXML
-    private TableColumn tableColumName;
+    private TableColumn<StationVo,String> tableColumName;
     @FXML
-    private TableColumn tableColumDate;
+    private TableColumn<StationVo,String> tableColumDate;
     @FXML
-    private TableColumn tableColumTarget;
+    private TableColumn<StationVo,String> tableColumTarget;
     @FXML
-    private TableColumn tableColumActual;
+    private TableColumn<StationVo,String> tableColumActual;
     @FXML
-    private TableColumn tableColumVariance;
+    private TableColumn<StationVo,String> tableColumVariance;
 
 
     public MonitorShowDataView(Object obj) {
@@ -46,16 +53,69 @@ public class MonitorShowDataView extends AMonitorView implements Initializable {
 
     @Override
     public void updateStationList(List<StationVo> list) {
+        ObservableList<StationVo> observableList = FXCollections.observableArrayList(list);
+        tableView.setItems(observableList);
+
+        tableColumName.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<StationVo, String>, ObservableValue<String>>()
+        {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<StationVo, String> param) {
+                return param.getValue().nameSSPProperty();
+            }
+        });
+        tableColumDate.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<StationVo, String>, ObservableValue<String>>()
+        {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<StationVo, String> param) {
+                return param.getValue().dateSSPProperty();
+            }
+        });
+        tableColumTarget.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<StationVo, String>, ObservableValue<String>>()
+        {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<StationVo, String> param) {
+                return param.getValue().targetSSPProperty();
+            }
+        });
+        tableColumActual.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<StationVo, String>, ObservableValue<String>>()
+        {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<StationVo, String> param) {
+                return param.getValue().actualSSPProperty();
+            }
+        });
+        tableColumVariance.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<StationVo, String>, ObservableValue<String>>()
+        {
+            @Override
+            public ObservableValue<String> call(TableColumn.CellDataFeatures<StationVo, String> param) {
+                return param.getValue().varianceSSPProperty();
+            }
+        });
+
+        /*tableColumName.setCellValueFactory(
+                new PropertyValueFactory<StationVo, String>("Name"));
+        tableColumDate.setCellValueFactory(
+                new PropertyValueFactory<StationVo, Date>("Date"));
+        tableColumTarget.setCellValueFactory(
+                new PropertyValueFactory<StationVo, Integer>("Target"));
+        tableColumActual.setCellValueFactory(
+                new PropertyValueFactory<StationVo, Integer>("Actual"));
+        tableColumVariance.setCellValueFactory(
+                new PropertyValueFactory<StationVo, Integer>("Variance"));
+*/
+
+
+
 
     }
 
     public void initialize(URL location, ResourceBundle resources) {
         EnterEventHandler enterHandler = new EnterEventHandler();
-
+    /*
         this.tableColumDate.setOnEditCommit(enterHandler);
         this.tableColumActual.setOnEditCommit(enterHandler);
         this.tableColumName.setOnEditCommit(enterHandler);
-
+    */
         this.changeViewBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
             public void handle(MouseEvent event) {
