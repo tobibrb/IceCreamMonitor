cf6ee423b4c38727ac79249a249c958b2f5417e5
Tobi@Tobi-PC <tobi.ffo@googlemail.com>
2015-12-01 10:33:17 +0100

    updated date regex

--- src/main/java/de/fhb/view/MonitorInsertDataView.java
+++ src/main/java/de/fhb/view/MonitorInsertDataView.java
@@ -210,7 +210,10 @@ public class MonitorInsertDataView extends AMonitorView implements Initializable
      * <code>false</code> falls String nicht dem Format entspricht.
      */
     private boolean validateDate(String date) {
-        return date.matches("([0-9]{2}).([0-9]{2}).([0-9]{4})");
+        return date.matches("^(((0?[1-9]|[12]\\d|3[01])[\\.\\-\\/](0?[13578]|1[02])[\\.\\-\\/]((1[6-9]|[2-9]\\d)?" +
+                "\\d{2}))|((0?[1-9]|[12]\\d|30)[\\.\\-\\/](0?[13456789]|1[012])[\\.\\-\\/]((1[6-9]|[2-9]\\d)?\\d{2}" +
+                "))|((0?[1-9]|1\\d|2[0-8])[\\.\\-\\/]0?2[\\.\\-\\/]((1[6-9]|[2-9]\\d)?\\d{2}))|(29[\\.\\-\\/]0?2[\\." +
+                "\\-\\/]((1[6-9]|[2-9]\\d)?(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00)|00)))$");
     }
 
     /**
