9619e6cecd1f78fe461493cccc84cc2a38aee564
Toni <Toni@172.16.25.207>
2015-12-01 15:46:32 +0100

    Zweite View kann jetzt gefiltert werden.

--- src/main/java/de/fhb/view/MonitorShowDataView.java
+++ src/main/java/de/fhb/view/MonitorShowDataView.java
@@ -2,24 +2,27 @@ package de.fhb.view;
 
 import de.fhb.model.StationVo;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 
 import java.net.URL;
-import java.util.Date;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 
 /**
  * Created by Tobi on 30.11.2015.
@@ -40,6 +43,10 @@ public class MonitorShowDataView extends AMonitorView implements Initializable {
     private TableColumn<StationVo,String> tableColumActual;
     @FXML
     private TableColumn<StationVo,String> tableColumVariance;
+    @FXML
+    private ChoiceBox<String> choiceBox;
+
+    private ObservableList<StationVo> observableList;
 
 
     public MonitorShowDataView(Object obj) {
@@ -53,8 +60,40 @@ public class MonitorShowDataView extends AMonitorView implements Initializable {
 
     @Override
     public void updateStationList(List<StationVo> list) {
-        ObservableList<StationVo> observableList = FXCollections.observableArrayList(list);
-        tableView.setItems(observableList);
+        if (list !=null) {
+            observableList = FXCollections.observableArrayList(list);
+
+        }
+        if (choiceBox.getSelectionModel().isSelected(0)) {
+            tableView.setItems(observableList);
+
+        }
+
+
+    }
+
+    public void filterTable(int choice){
+        FilteredList<StationVo> fList = new FilteredList<StationVo>(observableList, p-> true);
+            fList.setPredicate(station ->{
+                   switch (choice) {
+                       case 0:
+                           return true;
+                       case 1:
+                           return station.getActualValue()!=null && station.getActualValue() * 100 / station.getTargetValue() <= 90;
+                       case 2:
+                           return station.getActualValue()!=null && station.getActualValue() * 100 / station.getTargetValue() > 90 && station.getActualValue() * 100 / station.getTargetValue() < 105;
+                       case 3:
+                           return station.getActualValue()!=null && station.getActualValue() * 100 / station.getTargetValue() >= 105;
+                       default:
+                           return false;
+                   }
+            });
+        tableView.setItems(fList);
+
+
+    }
+
+    public void initialize(URL location, ResourceBundle resources) {
 
         tableColumName.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<StationVo, String>, ObservableValue<String>>()
         {
@@ -91,31 +130,33 @@ public class MonitorShowDataView extends AMonitorView implements Initializable {
                 return param.getValue().varianceSSPProperty();
             }
         });
+        ObservableList<String> observableChoichList = FXCollections.observableArrayList(Arrays.asList("Alle","-10% Abweichung","Zwischen -10% und +5%", "über +5%"));
+        this.choiceBox.setItems(observableChoichList);
+        this.choiceBox.getSelectionModel().selectFirst();
+        this.choiceBox.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (newValue != null){
+                    switch (newValue){
+                        case "-10% Abweichung":
+                            filterTable(1);
+                            break;
+                        case "Zwischen -10% und +5%":
+                            filterTable(2);
+                            break;
+                        case "über +5%":
+                            filterTable(3);
+                            break;
+                        default:
+                            filterTable(0);
+                            updateStationList(null);
 
-        /*tableColumName.setCellValueFactory(
-                new PropertyValueFactory<StationVo, String>("Name"));
-        tableColumDate.setCellValueFactory(
-                new PropertyValueFactory<StationVo, Date>("Date"));
-        tableColumTarget.setCellValueFactory(
-                new PropertyValueFactory<StationVo, Integer>("Target"));
-        tableColumActual.setCellValueFactory(
-                new PropertyValueFactory<StationVo, Integer>("Actual"));
-        tableColumVariance.setCellValueFactory(
-                new PropertyValueFactory<StationVo, Integer>("Variance"));
-*/
+                    }
+                }
+            }
+        });
 
 
-
-
-    }
-
-    public void initialize(URL location, ResourceBundle resources) {
-        EnterEventHandler enterHandler = new EnterEventHandler();
-    /*
-        this.tableColumDate.setOnEditCommit(enterHandler);
-        this.tableColumActual.setOnEditCommit(enterHandler);
-        this.tableColumName.setOnEditCommit(enterHandler);
-    */
         this.changeViewBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
             public void handle(MouseEvent event) {
