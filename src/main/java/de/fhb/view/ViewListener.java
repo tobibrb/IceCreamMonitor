85174fedaadc3a62890964bd2d0ba315db10e1a7
Toni <Toni@172.16.25.207>
2015-12-14 11:39:40 +0100

    added some Comments

--- src/main/java/de/fhb/view/ViewListener.java
+++ src/main/java/de/fhb/view/ViewListener.java
@@ -3,7 +3,7 @@ package de.fhb.view;
 import de.fhb.model.StationVo;
 
 /**
- * Interface zum erkenne von Änderung der View, wie Wechsel der View oder ändern der Daten durch den Benutzer über die View.
+ * Interface zum Erkennen von Änderungen über die View, wie Wechsel der View oder ändern der Daten durch den Benutzer über die View.
  */
 public interface ViewListener {
 
